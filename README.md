917bce94e3cd778f280190ddcec2c9d9ef907503
Tatsuro Moriyama <t4traw@gmail.com>
2018-02-01 14:00:30 +0900

    とりあえず基本的なユーザーセッションの分を追加

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ rails newする時に普段使うgemとか設定が面倒なので、Application
 
 ```
 $ mkdir YOUR_APP_NAME && cd $_
-$ rails new . -B -d postgresql --webpack=vue -m https://raw.github.com/likelivejp/rails_initializer/master/app_template.rb
+$ rails new . -B --database=postgresql --webpack=vue -m https://raw.github.com/likelivejp/rails_initializer/master/app_template.rb
 ```
 
 ## 参考
--- app_template.rb
+++ app_template.rb
@@ -18,13 +18,9 @@ gem "i18n-js"
 # 今開発環境だよってわかりやすく表示
 gem "rack-dev-mark"
 
+# webpackのコンパイルを開発時に自動化
 gem "foreman"
-
-# ユーザー認証系ライブラリ
-@sorcery = yes? "Would you like to install sorcery?"
-if @sorcery
-  gem "sorcery"
-end
+get "#{@repo_url}/Procfile", "Procfile"
 
 gem_group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
@@ -40,11 +36,40 @@ end
 
 run "bundle install"
 
+application <<-APPLICATION_CONFIG
+config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.jbuilder false
+end
+APPLICATION_CONFIG
+
+gem "sorcery"
+run "bundle install"
+run "rails generate sorcery:install remember_me reset_password"
+generate :scaffold, "email:string crypted_password:string salt:string --migration false"
+generate :controller, "UserSessions new create destroy"
+%w(
+  app/controllers/users_controller.rb
+  app/controllers/user_sessions_controller.rb
+  app/models/user.rb
+  app/views/layouts/application.html.erb
+  app/views/users/_form.html.erb
+  app/views/users/edit.html.erb
+  app/views/users/index.html.erb
+  app/views/users/new.html.erb
+  app/views/users/show.html.erb
+).each do |path|
+  run "rm #{path}"
+  get "#{@repo_url}/#{path}", path
+end
+
 after_bundle do
   run "curl https://www.gitignore.io/api/macos%2Crails > .gitignore"
   run "guard init minitest"
-  run "rails generate sorcery:install remember_me reset_password" if @sorcery
-  get "#{@repo_url}/Procfile", "Procfile"
+  rake "db:create"
+  rake "db:migrate"
   git :init
   git add: "."
   git commit: "-a -m 'rails new'"
