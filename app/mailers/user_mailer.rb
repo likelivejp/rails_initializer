76be1df6f592fdc47af01638a39fa99e59fb767b
Tatsuro Moriyama <t4traw@gmail.com>
2018-02-02 12:29:31 +0900

    メソッドがデフォルトのままだったので修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,20 +4,24 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.activation_needed_email.subject
   #
-  def activation_needed_email
-    @greeting = "Hi"
-    mail to: "to@example.org"
+  def activation_needed_email(user)
+    @user = user
+    @url  = "http://0.0.0.0:3000/users/#{user.activation_token}/activate"
+    mail(to: user.email, subject: 'Welcome to My Awesome Site')
   end
+
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.activation_success_email.subject
   #
-  def activation_success_email
-    @greeting = "Hi"
-    mail to: "to@example.org"
+  def activation_success_email(user)
+    @user = user
+    @url  = "http://0.0.0.0:3000/login"
+    mail(to: user.email, subject: 'Your account is now activated')
   end
+
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
