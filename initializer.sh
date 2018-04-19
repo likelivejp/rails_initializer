2c9be87d0e41741dd57a5680e2d5f1b00bc2f660
t4traw <t4traw@gmail.com>
2018-04-19 13:25:33 +0900

    upgrade rails ver 5.2.0

--- initializer.sh
+++ initializer.sh
@@ -1,3 +1,3 @@
 rbenv gemset init
-gem install rails -v 5.1.4
+gem install rails -v 5.2.0
 rails new . -B --database=postgresql --webpack=vue -m https://raw.github.com/likelivejp/rails_initializer/master/template.rb
