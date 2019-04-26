c597485de13c3bbde5d6d7ace962049bb2c4179b
Tatsuro Moriyama <t4traw@gmail.com>
2019-04-26 17:26:24 +0900

    Update rails 5.2.3

--- initializer.sh
+++ initializer.sh
@@ -1,3 +1,3 @@
 rbenv gemset init
-gem install rails -v 5.2.0
+gem install rails -v 5.2.3
 rails new . -B --database=postgresql --webpack=vue -m https://raw.github.com/likelivejp/rails_initializer/master/template.rb
