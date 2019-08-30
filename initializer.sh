7343117d2b1cafb2909b39e7b9eddd50fa063287
TatsuroMoriyama <t4traw@gmail.com>
2019-08-30 12:39:17 +0900

    Rails6に変更

--- initializer.sh
+++ initializer.sh
@@ -1,3 +1,3 @@
 rbenv gemset init
-gem install rails -v 5.2.3
-rails new . -B --database=postgresql --webpack=vue -m https://raw.github.com/likelivejp/rails_initializer/master/template.rb
+gem install rails -v 6.0.0
+rails new . -B --database=postgresql --webpack -m https://raw.github.com/likelivejp/rails_initializer/master/template.rb
