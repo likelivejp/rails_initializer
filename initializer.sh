eb2b40df535d3af549a5cc2d395a23fa875643c7
TatsuroMoriyama <t4traw@gmail.com>
2019-08-30 12:39:40 +0900

    ActionCableをスキップするよう変更

--- initializer.sh
+++ initializer.sh
@@ -1,3 +1,3 @@
 rbenv gemset init
 gem install rails -v 6.0.0
-rails new . -B --database=postgresql --webpack -m https://raw.github.com/likelivejp/rails_initializer/master/template.rb
+rails new . -B -C --database=postgresql --webpack -m https://raw.github.com/likelivejp/rails_initializer/master/template.rb
